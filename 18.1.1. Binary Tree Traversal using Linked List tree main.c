d9b824dc9ada94f2a12efd473db66e798c083368
Harsh Sharma <156127159+harshu1910@users.noreply.github.com>
2024-11-15 18:24:58 +0530

    18.1.1. Binary Tree Traversal using Linked List tree main.c

---
+++ 18.1.1. Binary Tree Traversal using Linked List tree main.c
@@ -0,0 +1,53 @@
+// Program for linked implementation of complete binary tree 
+                front->right = NULL; 
+            front->right = temp; 
+        }
+        // If the front node has both the left child and right child, 
+        // Dequeue() it. 
+        if (hasBothChild(front)) 
+        {
+                Dequeue(queue);
+        }
+    } 
+    // Enqueue() the new node for later insertions 
+            Enqueue(temp, queue); 
+} 
+  
+// Standard level order traversal to test above function 
+void levelOrder(struct node* root) 
+{ 
+    struct Queue* queue = createQueue(SIZE); 
+    Enqueue(root, queue); 
+    while (!isEmpty(queue)) 
+    { 
+        struct node* temp = Dequeue(queue); 
+        printf("%d ", temp->data); 
+        if (temp->left) 
+            Enqueue(temp->left, queue); 
+        if (temp->right) 
+            Enqueue(temp->right, queue); 
+    } 
+}
+
+// Driver program to test above functions 
+int main() 
+{
+    struct node* T1 = NULL; 
+    struct Queue* queue1 = createQueue(SIZE); 
+    int ele;
+   
+    while(1){
+       printf("Enter value : ");
+       scanf("%d",&ele);
+    if(ele==-1){
+        break;
+    } 
+    else{
+           insert(&T1, ele, queue1); 
+       }
+    }
+    inorder(T1);
+    preorder(T1);
+    postorder(T1);
+  
+}
