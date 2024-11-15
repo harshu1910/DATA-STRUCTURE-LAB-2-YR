706fead06f3da320239fbc1b0182090525507dc2
Harsh Sharma <156127159+harshu1910@users.noreply.github.com>
2024-11-15 18:23:06 +0530

    Binary tree traversals using Linked list postorder traversal .c

---
+++ Binary tree traversals using Linked list postorder traversal .c
@@ -0,0 +1,28 @@
+void inorder( struct node *root)
+{
+    if(root!= NULL){
+    	inorder(root->left);
+    	printf("%d ", root->data);
+    	inorder(root->right);
+    	
+    }
+}
+ 
+void preorder( struct node *root){
+	if(root != NULL){
+		printf("%d ", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void postorder(struct node *root)
+{
+	if(root!=NULL){
+		postorder(root->left);
+		postorder(root->right);
+		printf("%d ",root->data);
+	}
+    // write your code here to compute 
+    // and print postorder traversal
+}
